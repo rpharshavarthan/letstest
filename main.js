ffc36007e071ca038dc13d92a93c35e92357cccc
harsha varthan <66173033+rpharshavarthan@users.noreply.github.com>
2023-11-08 22:39:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
-  // Serve the HTML file
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.send("hello world");
+  //res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/.well-known/acme-challenge/', (req, res) => {
